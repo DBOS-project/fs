cfeeb8267a65deb78500b25f2197ed709a67e53e
askiad <athinagoras.sk@gmail.com>
2020-10-15 09:50:40 -0400

    bigfile schema

---
+++ stored_procedures/Empty/Empty.java
@@ -0,0 +1,17 @@
+import org.voltdb.*;
+import java.io.File;
+import java.io.RandomAccessFile;;
+
+/* 
+ * usage:
+ * exec Empty
+ */
+
+public class Empty extends VoltProcedure {
+    public VoltTable[] run ()
+		throws VoltAbortException {
+
+		return voltExecuteSQL();
+    }
+}
+		     
