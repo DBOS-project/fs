483bca4c9888e6a32fcf350b33c3ffcd6ba25989
dhong98 <dhong98@mit.edu>
2020-10-19 10:04:01 -0400

    same number of files

--- benchmarks/multi_user_multi_cores.cpp
+++ benchmarks/multi_user_multi_cores.cpp
@@ -7,9 +7,12 @@
 using namespace std; 
   
 void start_client(int user, int transactions, int all_users) { 
-    string cmd = "single_user/synchronous/read_bench/run_client.sh -u user" + to_string(user) + " -t " + to_string(transactions) + " > outputs/" + to_string(all_users) + "/user" + to_string(user) + "r.out";
+	string cmd = "single_user/synchronous/create_bench/run_client.sh -u user" + to_string(user) + " -t " + to_string(transactions) + " > outputs2/" + to_string(all_users) + "/user" + to_string(user) + "c.out";
     system(cmd.c_str());
-} 
+
+    cmd = "single_user/synchronous/populate_bench/run_client.sh -u user" + to_string(user) + " -t " + to_string(transactions) + " > outputs2/" + to_string(all_users) + "/user" + to_string(user) + "p.out";
+    system(cmd.c_str());
+}
 
 int main(int argc, char** argv) { 
     int users = atoi(argv[1]);
