490bbd056c8a67bf0f7198aaa18ebc7696d5190a
dhong98 <dhong98@mit.edu>
2020-10-19 20:16:23 -0400

    recompile jars

---
+++ benchmarks/multi_user/synchronous/write_bench/run_client.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+cd ${SCRIPT_DIR}
+
+CP="$CLASSPATH:$SCRIPT_DIR/../../../../VoltDB/voltdb/*"
+CP="$CP:$SCRIPT_DIR/../../../dependencies/commons-cli-1.4/*"
+
+SRC=`find client/src -name "*.java"`
+
+if [ ! -z "$SRC" ]; then
+    mkdir -p client/obj
+    javac -cp $CP -d client/obj $SRC
+    # stop if compilation fails
+    if [ $? != 0 ]; then exit; fi
+
+    jar cf client/client.jar -C client/obj .
+    rm -rf client/obj
+
+    java -cp "$CP:client/client.jar" org.voltdb.write.WriteBench $*
+fi
--- benchmarks/single_user/synchronous/read_bench/run_client.sh
+++ benchmarks/single_user/synchronous/read_bench/run_client.sh
@@ -6,4 +6,7 @@ cd ${SCRIPT_DIR}
 CP="$CLASSPATH:$SCRIPT_DIR/../../../../../VoltDB/voltdb/*"
 CP="$CP:$SCRIPT_DIR/../../../../dependencies/commons-cli-1.4/*"
 
+SRC=`find client/src -name "*.java"`
+
 java -cp "$CP:client/client.jar" org.voltdb.read.ReadBench $*
+
