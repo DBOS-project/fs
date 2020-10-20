9d7df87b91828142bd39a3b4f5d32b9897d07912
dhong98 <dhong98@mit.edu>
2020-10-19 20:16:49 -0400

    recompile jars

--- benchmarks/single_user/synchronous/populate_bench/run_client.sh
+++ benchmarks/single_user/synchronous/populate_bench/run_client.sh
@@ -6,6 +6,4 @@ cd ${SCRIPT_DIR}
 CP="$CLASSPATH:$SCRIPT_DIR/../../../../../VoltDB/voltdb/*"
 CP="$CP:$SCRIPT_DIR/../../../../dependencies/commons-cli-1.4/*"
 
-SRC=`find client/src -name "*.java"`
-
 java -cp "$CP:client/client.jar" org.voltdb.populate.PopulateBench $*
--- benchmarks/single_user/synchronous/read_bench/run_client.sh
+++ benchmarks/single_user/synchronous/read_bench/run_client.sh
@@ -6,7 +6,5 @@ cd ${SCRIPT_DIR}
 CP="$CLASSPATH:$SCRIPT_DIR/../../../../../VoltDB/voltdb/*"
 CP="$CP:$SCRIPT_DIR/../../../../dependencies/commons-cli-1.4/*"
 
-SRC=`find client/src -name "*.java"`
-
 java -cp "$CP:client/client.jar" org.voltdb.read.ReadBench $*
 
