8f5cdf9e3b57b64e9042fa212409ca0f15358c3d
dhong98 <dhong98@mit.edu>
2020-10-22 11:25:41 -0400

    run sequentially

--- benchmarks/multi_user_multi_cores.cpp
+++ benchmarks/multi_user_multi_cores.cpp
@@ -7,10 +7,7 @@
 using namespace std; 
   
 void start_client(int user, int transactions, int all_users) { 
-	string cmd = "single_user/synchronous/create_bench/run_client.sh -u user" + to_string(user) + " -t " + to_string(transactions) + " > outputs2/" + to_string(all_users) + "/user" + to_string(user) + "c.out";
-    system(cmd.c_str());
-
-    cmd = "single_user/synchronous/populate_bench/run_client.sh -u user" + to_string(user) + " -t " + to_string(transactions) + " > outputs2/" + to_string(all_users) + "/user" + to_string(user) + "p.out";
+	string cmd = "single_user/synchronous/read_bench/run_client.sh -u user" + to_string(user) + " -t " + to_string(transactions) + " > outputs3/" + to_string(all_users) + "/user" + to_string(user) + "r.out";
     system(cmd.c_str());
 }
 
