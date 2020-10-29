1b23034012fad74f0601032e085cbddebca8e3d6
askiad <athinagoras.sk@gmail.com>
2020-10-29 07:19:40 -0400

    client spawner

--- benchmarks/single_user_sync/write_bench/run_client.sh
+++ benchmarks/single_user_sync/write_bench/run_client.sh
@@ -6,5 +6,5 @@ cd ${SCRIPT_DIR}
 CP="$CLASSPATH:/home/gridsan/groups/DBOS/shared/VoltDB/voltdb-ent-9.3.2/voltdb/*"
 CP="$CP:$SCRIPT_DIR/../../../dependencies/commons-cli-1.4/*"
 
-java -cp "$CP:client/client.jar" org.voltdb.create.WriteBench $*
+java -cp "$CP:client/client.jar" org.voltdb.write.WriteBench $*
 
---
+++ scripts/benchmarks_single_sync/benchmark_create.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+#
+# usage:
+# ./benchmark_create.sh <output suffix> [flags]
+# flags: -h hostname, -t filecnt, -u username
+#
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+cd ${SCRIPT_DIR}/../..
+
+cd benchmarks/single_user_sync
+benchmarks="create_bench"
+
+# create files and populate them with 1MB data
+for bench in $benchmarks;
+do
+	cd $bench
+	./compile.sh
+	./run_client.sh ${*:2} &> ../../../testing/stats/single_$bench$1.out
+	cd ..
+done
+
