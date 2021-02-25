202ff4f3498767bff63359824594bc98dc274166
askiad <athinagoras.sk@gmail.com>
2021-02-25 11:29:51 -0500

    linux benchmarks

---
+++ linux_test/read_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./spawner read_files 40 $1 10 163840 10 _warmup_
+./spawner read_files 40 $1 10 163840 100 _read_4_2_1_1_
+./spawner read_files 40 $1 10 163840 100 _read_4_2_1_2_
+./spawner read_files 40 $1 10 163840 100 _read_4_2_1_3_
+./spawner read_files 40 $1 10 163840 100 _read_4_2_1_4_
+#./spawner read_files 40 $1 10 163840 300 _read_4_2_1_5_
